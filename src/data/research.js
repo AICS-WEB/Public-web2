f6179f2dc541fa60eff38dde6e63611ce7cbf63f
ppssj <psj3154728@gmail.com>
2026-08-05 15:26:23 +0900

    date 구분자 불일치 수정

--- src/data/research.js
+++ src/data/research.js
@@ -12,7 +12,9 @@ async function fetchPublicResource(path, signal) {
 }
 
 function formatProjectDate(value) {
-  const match = String(value || "").match(/^((?:19|20)\d{2})-(\d{2})/);
+  const match = String(value || "").match(
+    /^((?:19|20)\d{2})[.-](0[1-9]|1[0-2])/,
+  );
   return match ? `${match[1]}.${match[2]}` : "";
 }
 
